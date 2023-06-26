15944ab3d67a7e523946857d5cf771eb7df65668
ytemiloluwa <ytemiloluwa@gmail.com>
2023-06-26 14:12:26 +0100

    Added balance to SendView

--- BDKSwiftExampleWallet/View/SendView.swift
+++ BDKSwiftExampleWallet/View/SendView.swift
@@ -6,8 +6,111 @@
 //
 
 import SwiftUI
+import WalletUI
+import BitcoinDevKit
 
-class SendViewModel: ObservableObject {}
+class SendViewModel: ObservableObject {
+    
+    // Balance
+    @Published var balanceImmature: UInt64 = 0
+    @Published var balanceTrustedPending: UInt64 = 0
+    @Published var balanceUntrustedPending: UInt64 = 0
+    @Published var balanceConfirmed: UInt64 = 0
+    @Published var balanceTotal: UInt64 = 0
+    @Published var balanceSpendable: UInt64 = 0
+
+    // Sync
+    @Published var lastSyncTime: Date? = nil
+    @Published var walletSyncState: WalletSyncState = .notStarted
+    
+    // Transactions
+    @Published var transactionDetails: [TransactionDetails] = []
+    
+    func getBalance() {
+        do {
+            let balance = try BDKService.shared.getBalance()
+            self.balanceTotal = balance.total
+            self.balanceSpendable = balance.spendable
+            self.balanceImmature = balance.immature
+            self.balanceTrustedPending = balance.trustedPending
+            self.balanceUntrustedPending = balance.untrustedPending
+            self.balanceConfirmed = balance.confirmed
+        } catch let error as WalletError {
+            print("getBalance - Wallet Error: \(error.localizedDescription)")
+        } catch {
+            print("getBalance - Undefined Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func getTransactions() {
+        do {
+            let transactionDetails = try BDKService.shared.getTransactions()
+            self.transactionDetails = transactionDetails
+        } catch {
+            print("getTransactions - none: \(error.localizedDescription)")
+        }
+    }
+    
+    func sync() async {
+        DispatchQueue.main.async {
+            self.walletSyncState = .syncing
+        }
+        Task {
+            do {
+                try await BDKService.shared.sync()
+                DispatchQueue.main.async {
+                    self.walletSyncState = .synced
+                    self.lastSyncTime = Date()
+                    self.getBalance()
+                    self.getTransactions()
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.walletSyncState = .error(error)
+                }
+            }
+        }
+    }
+}
+
+extension SendViewModel {
+    
+    enum WalletSyncState: CustomStringConvertible, Equatable {
+        case error(Error)
+        case notStarted
+        case synced
+        case syncing
+        
+        var description: String {
+            switch self {
+            case .error(let error):
+                return "Error Syncing: \(error.localizedDescription)"
+            case .notStarted:
+                return "Not Started"
+            case .synced:
+                return "Synced"
+            case .syncing:
+                return "Syncing"
+            }
+        }
+        
+        static func ==(lhs: WalletSyncState, rhs: WalletSyncState) -> Bool {
+            switch (lhs, rhs) {
+            case (.error(let error1), .error(let error2)):
+                return error1.localizedDescription == error2.localizedDescription
+            case (.notStarted, .notStarted):
+                return true
+            case (.synced, .synced):
+                return true
+            case (.syncing, .syncing):
+                return true
+            default:
+                return false
+            }
+        }
+    }
+}
+
 
 struct SendView: View {
     @ObservedObject var viewModel: SendViewModel
@@ -18,10 +121,24 @@ struct SendView: View {
             Color(uiColor: .systemBackground)
                 .ignoresSafeArea()
             
-            VStack {
-                Text("Send")
+            VStack(spacing: 20) {
+                Text("Your Balance")
+                    .bold()
+                    .foregroundColor(.secondary)
+                HStack {
+                    Text(viewModel.balanceTotal.delimiter)
+                    Text("sats")
+                }
+                .font(.largeTitle)
+                Spacer()
             }
             .padding()
+            .onAppear {
+                viewModel.getBalance()
+            }
+            .task {
+                await viewModel.sync()
+            }
             
         }
         
